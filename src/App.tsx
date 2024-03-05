3a8dea388f70a22e0c1e3bce17d9e966561b3ae8
Sparrow48 <abunasib48@gmail.com>
2024-03-05 14:44:14 +0100

    Setup react todo app by webpack.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
+import './style.css';
+
 const App = () => {
   return (
-    <div>
+    <div className="main">
       <h1>React todo app!</h1>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
-import ReactDom from 'react-dom';
+import React from 'react';
 import App from './App';
 
-ReactDom.render(<App />, document.getElementById('root'));
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
